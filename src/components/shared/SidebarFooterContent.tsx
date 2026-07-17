2d31ad6fdeff43fbfa956f600180a3eb67d1072d
iPycc <ipycc@qq.com>
2026-07-18 02:32:08 +0800

    refactor(beian): 将备案logo源改为本地静态资源

--- src/components/shared/SidebarFooterContent.tsx
+++ src/components/shared/SidebarFooterContent.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils"
 import { useWebsiteSettings } from "./useWebsiteSettings"
 
-const FILING_LOGO_URL = "https://beian.mps.gov.cn/img/logo01.dd7ff50e.png"
+const FILING_LOGO_URL = "./beian.png"
 
 export function SidebarFooterContent({ className }: { className?: string }) {
   const settings = useWebsiteSettings()
